7ce8bec7a474d5a713ef27a5a992431a63004ce1
jaymspeights <jaymspeights@gmail.com>
2018-10-06 13:47:13 -0500

    added path engine skeleton

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 let Express = require('express');
 let app = Express();
+let path_engine = require('./path_engine.js');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/frontend/index.html');
 });
 
 app.get('/get/path', (req, res) =>  {
-
-    res.status(500).send('This endpoint has not been implemented!')
+    res.send(path_engine.getPath(req.query.a, req.query.b));
 });
 
 app.post('post/path', (req, res) => {
-
-    res.status(500).send('This endpoint has not been implemented!')
+    path_engine.addPath(req.body.path);
 });
 
-app.listen(8080);
+
+
+
+let PORT = 80;
+process.argv.forEach((val, index, array) => {
+    if (val === '-p') {
+        if (!array[index+1] || !parseInt(array[index+1])) {
+            console.error('Expected port after -p option');
+            process.exit(1);
+        }
+        PORT = array[index+1];
+    }
+});
+
+console.log(`Listening on ${PORT}`);
+app.listen(PORT);
---
+++ path_engine.js
@@ -0,0 +1,8 @@
+module.exports = {
+    getPath: function (a, b) {
+        return [a, b];
+    },
+    addPath: function (a, b) {
+
+    }
+};
