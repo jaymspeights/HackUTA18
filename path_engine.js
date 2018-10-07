203a86b68b78afbc0ab8cab64f8878331284c098
jaymspeights <jaymspeights@gmail.com>
2018-10-07 13:18:29 -0500

    balls deep

--- path_engine.js
+++ path_engine.js
@@ -126,7 +126,7 @@ function convertToGraph(graph) {
 }
 
 let MinHeap = require('min-heap');
-let h_scalar = 5;
+let h_scalar = 1;
 function aStar(graph, start, end) {
     let open_min = new MinHeap(function(l,r) {
         return l.f - r.f;
